0035dc1208c032a053447b760cf5dc2601d7ef5d
jmgtheworld <avedj94@hotmail.com>
2022-07-04 20:33:39 -0400

    add some imports and basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+// Components
+import Drawer from "@mui/material/Drawer";
+import LinearProgress from "@mui/material/LinearProgress";
+import Grid from "@mui/material/Grid";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import Badge from "@mui/material/Badge";
+
+// Styles
+import { Wrapper } from "./App.styles";
+
 const App = () => {
   return <div className="App">Start</div>;
 };
